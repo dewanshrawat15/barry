01b5a68039528784bf838a0e9f3a7971fdc5d9a3
Dewansh Rawat <dewanshrawat15@gmail.com>
2020-02-22 16:01:48 +0530

    Updated bot.py to display and generate multiple browser windows

--- bot.py
+++ bot.py
@@ -11,20 +11,24 @@ def init():
 	browser.get("https://google.com")
 	time.sleep(2)
 
+def windows(links):
+	counter = 0
+	for i in links:
+		browser.execute_script('''window.open(" ''' + i + '''", "_blank", "height=600,width=800,top=''' + str(counter) + ''', left=''' + str(counter) + '''");''')
+		counter = counter + 20
+
 def search(param):
 	searchBar = browser.find_element_by_name('q')
 	searchBar.send_keys(param)
 	searchBar.send_keys(Keys.ENTER)
 	time.sleep(2)
 	searchResults = browser.find_elements_by_class_name('r')
-	print(type(searchResults[0]))
-	print(len(searchResults))
 	links = []
 	for i in searchResults:
 		tempLinkElem = i.find_element_by_tag_name('a')
 		tempLink = tempLinkElem.get_attribute("href")
 		links.append(tempLink)
-	print(links)
+	windows(links)
 
 x = input("What would you like to search for? ")
 init()
